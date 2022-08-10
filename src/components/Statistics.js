e7f43fdec2b617b17ffb6d416f9c18e556dad7b1
LukasMercak <lukasthevicious@gmail.com>
2022-08-10 23:43:22 +0200

    Statistics

--- src/components/Statistics.js
+++ src/components/Statistics.js
@@ -13,14 +13,23 @@ export default function Statistics() {
     setIsShown(!isShown);
   };
   //Dodělat
-  useEffect(() => {}, [movies]);
+  useEffect(() => {
+    let avgRatingL = 0;
+    let avgRatingV = 0;
+    movies.forEach((movie) => {
+      avgRatingL = avgRatingL + movie.stars_1;
+      avgRatingV = avgRatingV + movie.stars_2;
+    });
+    setAverageRatingL(Math.round((avgRatingL / movies.length) * 10) / 10);
+    setAverageRatingV(Math.round((avgRatingV / movies.length) * 10) / 10);
+  }, [movies]);
 
   return (
     <div>
       <div className={isShown ? "statistics" : "statistics-hidden"}>
         <p>Movies watched: {movies.length}</p>
-        <p>Average rating L: </p>
-        <p>Average rating V:</p>
+        <p>Average rating L: {averageRatingL} </p>
+        <p>Average rating V: {averageRatingV} </p>
       </div>
       <FcStatistics
         className="statistics-icon"
